4ce5ee49b9da34d371ee5b96dc0bf058849fffa7
Sugih Mohammad Galih <59138555+smgalih19@users.noreply.github.com>
2021-10-08 17:48:15 +0700

    Selesai Video 3 - Struktur Folder, Routes & View

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about',[
+        "name" => "Sugih Mohammad Galih",
+        "email" => "smgalih19@gmail.com",
+        "image" => "smgalih19.jpg"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
