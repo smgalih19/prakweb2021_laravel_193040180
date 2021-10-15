af6c050442a06608c59b94d2235693382698b96b
Sugih Mohammad Galih <59138555+smgalih19@users.noreply.github.com>
2021-10-15 23:00:26 +0700

    Selesai Video 7 - Post Model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,37 +2,13 @@
 
 namespace App\Models;
 
-class Post
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
 {
-    private static $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Sugih Mohammad Galih",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae eius aut non accusamus provident praesentium, sit necessitatibus dolor fuga, architecto distinctio sapiente earum animi quidem unde veritatis in laborum officia asperiores nisi, numquam reprehenderit quo obcaecati! Explicabo voluptas veritatis, vel aperiam natus maiores amet at quae dolorum quos magni, veniam molestias quis nemo itaque, illum nam atque est animi. Incidunt quod qui iure ad. Eaque perferendis, iste distinctio sit assumenda explicabo omnis aliquid illum earum voluptates quidem voluptatem quas nostrum."
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Sugih Mohammad Galih",
-            "body" => "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias obcaecati modi placeat impedit atque, explicabo ipsum porro expedita distinctio aliquam dolorem temporibus? Obcaecati alias minima magni, pariatur accusamus, a provident, dolore temporibus iste vero asperiores. Velit, quos quae? At possimus iste eum velit ad error mollitia dignissimos, adipisci distinctio voluptate. Dolorem excepturi animi odit ea? Earum alias quia quas saepe quae expedita animi vel, ipsa dolor, iste dolorem maiores libero pariatur quibusdam itaque mollitia fuga vero qui sapiente dolores laboriosam voluptatem! Debitis assumenda voluptates placeat harum optio porro sint veniam tenetur ipsa? Provident adipisci quam quo ut deleniti molestiae voluptatum."
-        ]
-    ];
+    use HasFactory;
 
-    public static function all()
-    {
-        return collect(self::$blog_posts);
-    }
-
-    public static function find($slug)
-    {
-        $posts = static::all();
-        //     $post = [];
-        //     foreach($post as $p){
-        //         if($p["slug"] == $slug){
-        //             $post = $p;
-        //         }
-        //     }
-        return $posts->firstWhere('slug', $slug);
-    }
+    // protected $fillable = ['title', 'excerpt', 'body'];
+    protected $guarded = ['id'];
 }
