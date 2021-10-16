c32da816b261b1c24bd15e0ad1bf0adfc41aea52
Sugih Mohammad Galih <59138555+smgalih19@users.noreply.github.com>
2021-10-16 07:50:06 +0700

    Selesai Video 8 - Post Category & Eloquent Relationship

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,6 +3,8 @@
 @section('container')
   <h2 class="mb-5">{{ $post->title }}</h2>
   
+  <p>By. Sugih Mohammad Galih in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+
   {!! $post->body !!}
 
 <a href="/blog">Back to Posts</a>
