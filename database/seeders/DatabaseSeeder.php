53b1d0313619cd0ddfc5bd86d7d3e6890e37bdfa
Sugih Mohammad Galih <59138555+smgalih19@users.noreply.github.com>
2021-10-22 14:05:52 +0700

    Selesai Video 9 - Database Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +17,64 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        User::create([
+            'name' => 'Sugih Mohammad Galih',
+            'email' => 'smgalih@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        User::create([
+            'name' => 'Ujang Bach',
+            'email' => 'ujangbach@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        Category::create([
+            'name' => "Web Programming",
+            'slug' => 'web-programming'
+        ]);
+
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        Post::create([
+            'title' => 'Judul Pertama',
+            'slug' => 'judul-pertama',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur ab nobis esse illo ex iusto excepturi officia? Repellendus magni eius optio,',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur ab nobis esse illo ex iusto excepturi officia? Repellendus magni eius optio, similique possimus facilis dolore? Corporis molestias, suscipit consectetur repudiandae omnis ea commodi quos sequi! Asperiores molestias fuga aliquid earum tempora ipsum ab est aut, dolores, natus vitae saepe doloremque minima soluta amet nulla adipisci recusandae quasi, explicabo distinctio velit nesciunt facilis. Aperiam iste consequatur laudantium unde, asperiores perspiciatis, possimus quis ipsa, voluptatum mollitia vitae? Maxime minus voluptas doloribus consequuntur iusto praesentium tempora. Enim, maiores tempora velit totam vel officia neque tenetur quasi ducimus aspernatur, quos, eius esse autem? Ipsam?',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ke Dua',
+            'slug' => 'judul-ke-dua',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur ab nobis esse illo ex iusto excepturi officia? Repellendus magni eius optio,',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur ab nobis esse illo ex iusto excepturi officia? Repellendus magni eius optio, similique possimus facilis dolore? Corporis molestias, suscipit consectetur repudiandae omnis ea commodi quos sequi! Asperiores molestias fuga aliquid earum tempora ipsum ab est aut, dolores, natus vitae saepe doloremque minima soluta amet nulla adipisci recusandae quasi, explicabo distinctio velit nesciunt facilis. Aperiam iste consequatur laudantium unde, asperiores perspiciatis, possimus quis ipsa, voluptatum mollitia vitae? Maxime minus voluptas doloribus consequuntur iusto praesentium tempora. Enim, maiores tempora velit totam vel officia neque tenetur quasi ducimus aspernatur, quos, eius esse autem? Ipsam?',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ke Tiga',
+            'slug' => 'judul-ke-tiga',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur ab nobis esse illo ex iusto excepturi officia? Repellendus magni eius optio,',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur ab nobis esse illo ex iusto excepturi officia? Repellendus magni eius optio, similique possimus facilis dolore? Corporis molestias, suscipit consectetur repudiandae omnis ea commodi quos sequi! Asperiores molestias fuga aliquid earum tempora ipsum ab est aut, dolores, natus vitae saepe doloremque minima soluta amet nulla adipisci recusandae quasi, explicabo distinctio velit nesciunt facilis. Aperiam iste consequatur laudantium unde, asperiores perspiciatis, possimus quis ipsa, voluptatum mollitia vitae? Maxime minus voluptas doloribus consequuntur iusto praesentium tempora. Enim, maiores tempora velit totam vel officia neque tenetur quasi ducimus aspernatur, quos, eius esse autem? Ipsam?',
+            'category_id' => 2,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ke Empat',
+            'slug' => 'judul-ke-empat',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur ab nobis esse illo ex iusto excepturi officia? Repellendus magni eius optio,',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur ab nobis esse illo ex iusto excepturi officia? Repellendus magni eius optio, similique possimus facilis dolore? Corporis molestias, suscipit consectetur repudiandae omnis ea commodi quos sequi! Asperiores molestias fuga aliquid earum tempora ipsum ab est aut, dolores, natus vitae saepe doloremque minima soluta amet nulla adipisci recusandae quasi, explicabo distinctio velit nesciunt facilis. Aperiam iste consequatur laudantium unde, asperiores perspiciatis, possimus quis ipsa, voluptatum mollitia vitae? Maxime minus voluptas doloribus consequuntur iusto praesentium tempora. Enim, maiores tempora velit totam vel officia neque tenetur quasi ducimus aspernatur, quos, eius esse autem? Ipsam?',
+            'category_id' => 2,
+            'user_id' => 2
+        ]);
+
     }
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,9 +3,9 @@
 @section('container')
   <h2 class="mb-5">{{ $post->title }}</h2>
   
-  <p>By. Sugih Mohammad Galih in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+  <p>By. <a href="#" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
 
   {!! $post->body !!}
 
-<a href="/blog">Back to Posts</a>
+<a href="/blog" class="d-block mt-3">Back to Posts</a>
 @endsection
