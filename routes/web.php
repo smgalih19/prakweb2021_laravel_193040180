c7aead54bf2c5e8c6fb3d72a111879cfa06783b3
Sugih Mohammad Galih <59138555+smgalih19@users.noreply.github.com>
2021-10-29 16:56:23 +0700

    Selesai Video 13 - Searching & Pagination

--- routes/web.php
+++ routes/web.php
@@ -19,13 +19,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home', [
-        "title" => "Home"
+        "title" => "Home",
+        "active" => 'home'
     ]);
 });
 
 Route::get('/about', function () {
     return view('about',[
         "title" => "About",
+        "active" => "active",
         "name" => "Sugih Mohammad Galih",
         "email" => "smgalih19@gmail.com",
         "image" => "smgalih19.jpg"
@@ -33,7 +35,7 @@ Route::get('/about', function () {
 });
 
 
-Route::get('/blog', [PostController::class, 'index']);
+Route::get('/posts', [PostController::class, 'index']);
 
 Route::get('/posts/{post:slug}', [PostController::class, 'show']);
 
@@ -42,20 +44,5 @@ Route::get('/categories', function () {
         'title' => 'Post Categories',
         'active' => 'categories',
         'categories' => Category::all(),
-    ]);
-});
-
-Route::get('/categories/{category:slug}', function (Category $category) {
-    return view('posts',[
-        'title' => "Post By Category : $category->name",
-        'active' => 'categories',
-        'posts' => $category->posts->load('category','author')
-    ]);
-});
-
-Route::get('/authors/{author:username}', function(User $author){
-    return view('posts',[
-        'title' => "Post By Author : $author->name",
-        'posts' => $author->posts->load('category','author'),
     ]);
 });
