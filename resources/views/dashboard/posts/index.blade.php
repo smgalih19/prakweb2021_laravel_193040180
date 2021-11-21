2983896975ce7418ea500c1f3d11ed4c5e740ed5
Sugih Mohammad Galih <59138555+smgalih19@users.noreply.github.com>
2021-11-21 17:51:33 +0700

    Selesai Video 19 - Validation & Insert Post

--- resources/views/dashboard/posts/index.blade.php
+++ resources/views/dashboard/posts/index.blade.php
@@ -4,6 +4,13 @@
   <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
     <h1 class="h2">My Posts</h1>
   </div>
+
+  @if (session()->has('success'))
+      <div class="alert alert-success" role="alert">
+        {{ session('success') }}
+      </div>
+  @endif
+
   <div class="table-responsive col-lg-8">
     <a href="/dashboard/posts/create" class="btn btn-primary mb-3">Create new post</a>
     <table class="table table-striped table-sm">
@@ -22,7 +29,7 @@
             <td>{{ $post->title }}</td>
             <td>{{ $post->category->name }}</td>
             <td>
-              <a href="/dashboard/posts/{{ $posts->slug }}" class="badge bg-info"><span data-feather="eye"></span></a>
+              <a href="/dashboard/posts/{{ $post->slug }}" class="badge bg-info"><span data-feather="eye"></span></a>
               <a href="" class="badge bg-warning"><span data-feather="edit"></span></a>
               <a href="" class="badge bg-danger"><span data-feather="x-circle"></span></a>
             </td>
